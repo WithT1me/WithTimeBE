62a1d6f22581be70c841780e2519d6f6d894e45b
Jeongmo Seo <wjdah1402@gmail.com>
2025-05-23 14:33:35 +0900

    :rocket: chore: 프로젝트 초기 세팅

---
+++ src/main/java/org/withtime/be/withtimebe/WithTimeBeApplication.java
@@ -0,0 +1,13 @@
+package org.withtime.be.withtimebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithTimeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithTimeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/withtime/be/withtimebe/WithTimeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.withtime.be.withtimebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithTimeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
